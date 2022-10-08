1604535ccd2aeb4c0bb71356e43ac4bdcd364174
toom1996 <1023150697@qq.com>
2022-10-08 17:36:18 +0800

    commit

--- src/view/styles/Index.vue
+++ src/view/styles/Index.vue
@@ -3,7 +3,7 @@
     <div class="bg-white">
       <div>
         <main class="mx-auto">
-          <div class="flex items-baseline justify-between border-b border-gray-200 pt-24 pb-6">
+          <div class="flex items-baseline justify-between border-b border-gray-200 pt-4 pb-6">
             <h1 class="text-4xl font-bold tracking-tight text-gray-900">New Arrivals</h1>
 
             <div class="flex items-center">
@@ -74,8 +74,33 @@
               <!-- Product grid -->
               <div class="lg:col-span-3">
                 <!-- Replace with your content -->
-                <div class="h-96 rounded-lg border-4 border-dashed border-gray-200 lg:h-full" />
+<!--                <div class="h-96 rounded-lg border-4 border-dashed border-gray-200 lg:h-full" />-->
                 <!-- /End replace -->
+                <div class="bg-white">
+                  <div class="mx-auto max-w-2xl lg:max-w-7xl">
+<!--                    <h2 class="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>-->
+
+                    <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                      <div v-for="product in products" :key="product.id" class="group relative">
+                        <div class="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none">
+                          <img :src="product.imageSrc" :alt="product.imageAlt" class="h-full w-full object-cover object-center lg:h-full lg:w-full" />
+                        </div>
+                        <div class="mt-4 flex justify-between">
+                          <div>
+                            <h3 class="text-sm text-gray-700">
+                              <a :href="product.href">
+                                <span aria-hidden="true" class="absolute inset-0" />
+                                {{ product.name }}
+                              </a>
+                            </h3>
+                            <p class="mt-1 text-sm text-gray-500">{{ product.color }}</p>
+                          </div>
+                          <p class="text-sm font-medium text-gray-900">{{ product.price }}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </section>
@@ -100,6 +125,55 @@ import {
 } from '@headlessui/vue'
 import { XMarkIcon } from '@heroicons/vue/24/outline'
 import { ChevronDownIcon, FunnelIcon, MinusIcon, PlusIcon, Squares2X2Icon } from '@heroicons/vue/20/solid'
+
+const products = [
+  {
+    id: 1,
+    name: 'Basic Tee',
+    href: '#',
+    imageSrc: 'https://aliyun-img.hypebeast.cn/https%3A%2F%2Fhbx.hypebeast.com%2Ffiles%2F2022%2F04%2FED_16x9_01-1.jpg?q=85',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '$35',
+    color: 'Black',
+  },
+  {
+    id: 1,
+    name: 'Basic Tee',
+    href: '#',
+    imageSrc: 'https://imgf1.pop-fashion.com/upload/flash_report/2022/2022091622/ANAREPORT_63248aa86adee_9660.jpg',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '$35',
+    color: 'Black',
+  },
+  {
+    id: 1,
+    name: 'Basic Tee',
+    href: '#',
+    imageSrc: 'https://imgf2.pop-fashion.com/upload/analysisreport/2017/2017053117/ANAREPORT_592e8ce0c73da_8678.jpg',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '$35',
+    color: 'Black',
+  },
+  {
+    id: 1,
+    name: 'Basic Tee',
+    href: '#',
+    imageSrc: 'https://imgf3.pop-fashion.com/upload/analysisreport/2017/2017062817/ANAREPORT_5953780ada439_7997.jpg',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '$35',
+    color: 'Black',
+  },
+  {
+    id: 1,
+    name: 'Basic Tee',
+    href: '#',
+    imageSrc: 'https://imgf2.pop-fashion.com/upload/analysisreport/2017/2017050812/ANAREPORT_590ff21f3ab8a_8976.jpg',
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: '$35',
+    color: 'Black',
+  },
+  // More products...
+]
 
 const sortOptions = [
   { name: 'Most Popular', href: '#', current: true },
